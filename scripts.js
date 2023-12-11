55ca881e41613f72fecb156634e26f597c780b59
Milly <lauraa9lemos@gamail.com>
2023-12-11 18:21:44 -0300

    Feat: adicionei um carocel nos meus Projetos

--- scripts.js
+++ scripts.js
@@ -1,9 +1,78 @@
-let header = document.getElementById('header')
+// const menuHamburguer = document.querySelector('.menu-hamburguer');
+// const nav = document.querySelector('.navbar');
+// const links = document.querySelectorAll('.navbar-links li a');
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= 200) {
-        header.style.background = '#191919'
-    } else {
-        header.style.background = 'transparent'
-    }
-})
+// let swiper = createSwiper(".mySwiper", ".swiper-pagination", ".swiper-button-next", ".swiper-button-prev");
+// let header = document.getElementById('header');
+
+// function createSwiper(container, pagination, nextButton, prevButton) {
+//     return new Swiper(container, {
+//         slidesPerView: handleWidth(),
+//         spaceBetween: 30,
+//         pagination: {
+//             el: pagination,
+//             clickable: true,
+//         },
+//         navigation: {
+//             nextEl: nextButton,
+//             prevEl: prevButton,
+//         },
+//     });
+// }
+
+// function handleWidth() {
+//     let getWidth = window.innerWidth || document.documentElement.clientWidth;
+//     let slideShow = 3;
+
+//     if (getWidth < 1001) {
+//         slideShow = 2;
+//     }
+
+//     if (getWidth < 700) {
+//         slideShow = 1;
+//     }
+
+//     return slideShow
+// }
+
+// menuHamburguer.addEventListener('click', () => {
+//     nav.classList.toggle('active');
+// });
+
+// links.forEach(item => {
+//     item.addEventListener('click', () => {
+//         nav.classList.toggle('active');
+//     })
+// })
+
+// window.addEventListener('resize', () => {
+//     swiper.params.slidesPerView = handleWidth();
+//     swiper.update();
+// })
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+            el: '.swiper-pagination', // Corrigir o nome da classe para swiper-pagination
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next", // Corrigir o nome da classe para swiper-button-next
+            prevEl: ".swiper-button-prev", // Corrigir o nome da classe para swiper-button-prev
+        },
+    });
+
+    let header = document.getElementById("header");
+
+    window.addEventListener('scroll', () => {
+        if (document.documentElement.scrollTop >= 200) {
+            header.style.background = '#191919';
+        } else {
+            header.style.background = 'transparent';
+        }
+    });
+});
